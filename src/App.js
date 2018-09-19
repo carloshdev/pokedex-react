ab307391da98881af6cac5e4802b6ff3f11a337d
Carlos Henrique <carloshm.ribeiro@gmail.com>
2018-09-19 03:54:41 -0300

    Foi feito alteração visual, inclusão de imagem, inclusão de um formato card, tudo está sendo exibido na tela

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import { Input, Label } from 'reactstrap';
+import { Input, Label, Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 class App extends Component {
   
 constructor() {
   super();
   this.state = {
-    vetId: [7, 6,25,132,249],
-    vetPokemon: []
+    vetId: ['squirtle', 6, 25, 132, 249],
+    vetPokemon: [],
+    pokemonSelecionado:null,
+    idSelecionado:-1
   }  
   this.getPokeApi();
+  
 }
 
 getPokeApi = () => {
@@ -25,8 +29,7 @@ this.state.vetId.forEach(id => {
       if(erro){
           console.log(erro);
       }
-  })
-  
+  })  
 });
     //teste
     /*this.vetPokemon[1] = axios.get("http://pokeapi.co/api/v2/pokemon/25")
@@ -34,21 +37,63 @@ this.state.vetId.forEach(id => {
     this.vetPokemon[3] = axios.get("http://pokeapi.co/api/v2/pokemon/132")
     this.vetPokemon[4] = axios.get("http://pokeapi.co/api/v2/pokemon/249")
 */
+
 }
 
+changePokemon = (id) =>{
+  if(id == -1){
+    this.setState({idSelecionado: id});
+    this.setState({pokemonSelecionado: null});
+    return;
+  }
+  this.state.vetPokemon.map((pokemon) => { 
+    
+    if(pokemon.id == id){
+      this.setState({pokemonSelecionado: pokemon});
+      this.setState({idSelecionado: pokemon.id});
+    }
+  
+})             
+  
+};
+
   render() {
+    if (this.state.vetPokemon.length < this.state.vetId.length-1){
+      return <div>Carregando...</div>
+  }
+
     return (
       <div className="App">
-        
-        <Label for="exampleSelect">Select</Label>
-          <Input type="select" name="select" id="exampleSelect" onChange={(value)=>alert(value.value)}>
+          <div style={{width:'50%'}}>
+          <Label for="select">Selecione um Pokemon</Label> 
+            <Input type="select" name="select" value={this.state.idSelecionado} id="select" onChange={(input)=>this.changePokemon(input.target.value)}>
+            <option key={-1} value={-1} >Selecione um Pokemon</option>
             {
-              this.state.vetPokemon.map((pokemon) => {
-                  return <option >{pokemon.name}</option>;
-              })
+              this.state.vetPokemon.map((pokemon) => { 
+                  return <option key={pokemon.id} value={pokemon.id} >{pokemon.name}</option>;
+              })             
             } 
-          </Input>
-        
+          </Input> 
+          </div>
+          
+          <div style={{width:'50%'}}>
+          {
+            
+            this.state.pokemonSelecionado ?
+              <Card>
+                <CardImg top style={{width:"50%"}} src={this.state.pokemonSelecionado.sprites.front_default} alt="Card image cap" />
+                <CardBody>
+                <CardTitle>{this.state.pokemonSelecionado.name.toUpperCase()} - {this.state.pokemonSelecionado.id}</CardTitle>
+                <CardSubtitle>{this.state.pokemonSelecionado.types[0].type.name.toUpperCase()}</CardSubtitle>
+                
+                </CardBody>
+                </Card> 
+                :
+                ""
+          }
+                 
+          </div>
+           
       </div>
     );
   }
